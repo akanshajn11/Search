e708325f477cf35c8cfc73c6c7a59e78309343d4
Akansha Jain <akansha.jn111@gmail.com>
2024-04-05 10:26:25 +0200

    Initial commit

---
+++ app/src/main/java/com/akansha/mvvm/model/SampleData.kt
@@ -0,0 +1,32 @@
+package com.akansha.mvvm.model
+
+class SampleData {
+
+    companion object {
+        val usersList = arrayListOf(
+            "Alice", "Bob", "Charlie", "David", "Eve",
+            "Frank", "Grace", "Henry", "Isabel", "Jack",
+            "Kate", "Liam", "Mia", "Nathan", "Olivia",
+            "Patrick", "Quinn", "Rachel", "Samuel", "Tina",
+            "Uma", "Victor", "Wendy", "Xavier", "Yara",
+            "Zachary", "Abigail", "Benjamin", "Catherine", "Daniel",
+            "Emily", "Finn", "Gabriella", "Hannah", "Ian",
+            "Jessica", "Kevin", "Lily", "Michael", "Nora",
+            "Oscar", "Penelope", "Quentin", "Rebecca", "Sarah",
+            "Thomas", "Ursula", "Violet", "William"
+        )
+
+        val brandsList = arrayListOf(
+            "Apple", "Nike", "Samsung", "Adidas", "Google",
+            "Amazon", "Microsoft", "Coca-Cola", "Toyota", "Mercedes-Benz",
+            "BMW", "Louis Vuitton", "Disney", "McDonald's", "Sony",
+            "Intel", "Facebook", "Cisco", "Oracle", "Gucci",
+            "H&M", "Netflix", "Pepsi", "Visa", "Mastercard",
+            "Starbucks", "Walmart", "IBM", "Nestle", "Ford",
+            "Honda", "Loreal", "Canon", "Burberry", "Colgate",
+            "Kellogg's", "Hyundai", "Panasonic", "UPS", "FedEx",
+            "Audi", "Porsche", "Volkswagen", "Lexus", "Prada",
+            "Rolex", "Chanel", "Bentley", "Hermes"
+        )
+    }
+}
